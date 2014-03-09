c4f543f5e838dc531ad006a0505f8631d345c5f9
Vasily <vasilij.ershov@gmail.com>
2014-03-09 16:19:11 +0400

    my part of splitting news to sentence

---
+++ src/main/java/ParseNews.java
@@ -0,0 +1,35 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+
+/**
+ * User: Vasily
+ * Date: 09.03.14
+ * Time: 15:38
+ */
+public class ParseNews {
+    public static void main(String[] args) {
+        try {
+            Parser parser = new Parser("/tmp/news");
+            LinkedList<String> result = parser.parse();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("/tmp/result"));
+            writer.write("{\n" +
+                    "\t\"document\": {\n" +
+                    "\t\t\"sentences\": \n" +
+                    "\t\t[");
+            for (String sentence: result) {
+                writer.write("\"" + sentence + "\",");
+            }
+            writer.write("]\n" +
+                    "\t\t}\n" +
+                    "\n" +
+                    "}");
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+
+        }
+
+    }
+}
