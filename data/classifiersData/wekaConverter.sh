9c1a77bd2f16c75beb2659e7a10033f651569243
Vasily <vasilij.ershov@gmail.com>
2014-04-07 11:06:08 +0400

    classification report

--- data/classifiersData/wekaConverter.sh
+++ data/classifiersData/wekaConverter.sh
@@ -10,4 +10,8 @@ mv $2.tmp.arff $2.arff
 java -classpath weka.jar  weka.filters.unsupervised.attribute.Reorder -R 2-last,1 -b -i $1.arff  -o  $1.tmp.arff -r  $2.arff -s $2.tmp.arff
 mv $1.tmp.arff $1.arff
 mv $2.tmp.arff $2.arff
-#java -classpath weka.jar weka.filters.supervised.attribute.AttributeSelection -E "weka.attributeSelection.ChiSquaredAttributeEval" -S "weka.attributeSelection.Ranker -T 0 -N -1" -b -i /Users/Vasily/Dropbox/homework/NLP/nlp-compscicenter-2014/data/classifiersData/10classes/learn2.arff  -o learnFinal.arff -r /Users/Vasily/Dropbox/homework/NLP/nlp-compscicenter-2014/data/classifiersData/10classes/test2.arff -s /Users/Vasily/Dropbox/homework/NLP/nlp-compscicenter-2014/data/classifiersData/10classes/testFinal.arff
+java -classpath weka.jar  weka.filters.unsupervised.attribute.ClassAssigner -C last  -b -i $1.arff  -o  $1.tmp.arff -r  $2.arff -s $2.tmp.arff
+mv $1.tmp.arff $1.arff
+mv $2.tmp.arff $2.arff
+#java -classpath weka.jar weka.filters.supervised.attribute.AttributeSelection -E "weka.attributeSelection.ChiSquaredAttributeEval" -S "weka.attributeSelection.Ranker -T 0 -N -1" -b -i $1.arff  -o $1Final.arff -r $2.arff -s $2Final.arff
+java -classpath weka.jar  weka.filters.unsupervised.attribute.RandomProjection -N 400 -R 42 -D Sparse1 -c last -b -i $1.arff  -o $1Proj.arff -r $2.arff -s $2Proj.arff
